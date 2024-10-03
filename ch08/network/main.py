e5c7d1159dc9967d34a8c8f28aea6d2c4ea28e86
mandeukJeong <wjdalstj137@gmail.com>
2024-10-03 22:59:32 +0900

    동적 계획법 - 네트워크 선 자르기(top-down)

--- ch08/network/main.py
+++ ch08/network/main.py
@@ -1,10 +1,16 @@
 import sys
 sys.stdin = open("input.txt", 'r')
-n=int(input())
-dy=[0]*(n+1)
-dy[1]=1
-dy[2]=2
-for i in range(3, n+1):
-    dy[i]=dy[i-1]+dy[i-2]
 
-print(dy[n])
+def DFS(len):
+    if dy[len]>0:
+        return dy[len]
+    if len==1 or len==2:
+        return len
+    else:
+        dy[len]=DFS(len-1)+DFS(len-2)
+        return dy[len]
+
+if __name__=="__main__":
+    n=int(input())
+    dy=[0]*(n+1)
+    print(DFS(n))
